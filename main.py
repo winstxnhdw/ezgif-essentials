015b51f3c20035abdbacfb2619179f2a831bb592
winstxnhdw <winstxnhdw@gmail.com>
2022-09-01 06:56:28 +0800

    update

--- main.py
+++ main.py
@@ -24,25 +24,63 @@ def main():
 
     args, _ = parse_args()
 
-    if not args.assemble:
-        convert = ConvertVideo(args.input, args.transparent)
-
-    else:
-        convert = ConvertSequence(args.input, args.transparent, args.fps)
-
-    generate_gif(convert, args)
+    generate_gif(
+        ConvertVideo(args.input, args.transparent) if args.assemble else ConvertSequence(args.input, args.fps),
+        args
+    )
 
 def parse_args() -> tuple[Namespace, list[str]]:
 
     parser = ArgumentParser(description='Converts video/sequence to GIF')
-    parser.add_argument('-i', '--input', type=str, metavar='', help='Input file path', required=True)
 
-    parser.add_argument('-w', '--transparent', action='store_true', help='Enable transparency', required=False)
-    parser.add_argument('-l', '--lossy', type=str, default=None, metavar='', help='No. of artefacts allowed', required=False)
-    parser.add_argument('-z', '--optimise', type=int, default=None, metavar='', help='Optimise GIF file size (1 - 3)', required=False)
+    parser.add_argument(
+        '-i', '--input',
+        type=str,
+        metavar='',
+        help='Input file path',
+        required=True
+    )
+
+    parser.add_argument(
+        '-w', '--transparent',
+        action='store_true',
+        help='Enable transparency',
+        required=False
+    )
+
+    parser.add_argument(
+        '-a', '--assemble',
+        action='store_true',
+        help='Assemble image sequence',
+        required=False
+    )
+
+    parser.add_argument(
+        '-l', '--lossy',
+        type=str,
+        default=None,
+        metavar='',
+        help='No. of artefacts allowed',
+        required=False
+    )
+
+    parser.add_argument(
+        '-z', '--optimise',
+        type=int,
+        default=None,
+        metavar='',
+        help='Optimise GIF file size (1 - 3)',
+        required=False
+    )
     
-    parser.add_argument('-a', '--assemble', action='store_true', help='Assemble image sequence', required=False)
-    parser.add_argument('-r', '--fps', type=int, default=50, metavar='', help='Sequence FPS', required=False)
+    parser.add_argument(
+        '-r', '--fps',
+        type=int,
+        default=50,
+        metavar='', 
+        help='Sequence FPS', 
+        required=False
+    )
 
     return parser.parse_known_args()
 
